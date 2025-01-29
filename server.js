c3e104c3bcecbd49e7eed467626b25e900b86f6f
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2025-01-29 11:20:17 -0500

    updates

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const authRoutes = require("./routes/authRoutes");
 const tradeRoutes = require("./routes/tradeRoutes");
 const tradePlanRoutes = require("./routes/tradePlanRoutes");
 const tradeReviewRoutes = require("./routes/tradeReviewRoutes");
-const insiderTradeRoutes = require("./routes/insiderTradeRoutes");
 
 dotenv.config();
 connectDB();
@@ -23,7 +22,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/trades", tradeRoutes);
 app.use("/api/trade-plans", tradePlanRoutes);
 app.use("/api/trade-reviews", tradeReviewRoutes);
-app.use("/api/insider-trades", insiderTradeRoutes);
 
 // Base route
 app.get("/", (req, res) => res.send("API is running..."));
