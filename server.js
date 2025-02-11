6615ac6447fcc91c016148edccd35f1fb0cf3c9a
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2025-02-11 14:46:01 -0500

    updates

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN || "https://rivyl.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
