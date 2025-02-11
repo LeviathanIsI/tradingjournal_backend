0e8a72a5ad9986d8e4f1126b2dd467f1c56bbaa6
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2025-02-11 11:57:36 -0500

    updates

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000"],
-    credentials: true, // Allow cookies and headers
+    origin: ["*"],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
